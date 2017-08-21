2f77c25317e52cb7d1b73c15c300b78e3b3acb39
Camden Ko <camdenko@gmail.com>
2017-08-21 08:48:44 -0500

    scaffolding

--- src/Screens/Landing.js
+++ src/Screens/Landing.js
@@ -1,1 +1,12 @@
 import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+
+import theme from '../theme'
+
+export default () => (
+  <ThemeProvider theme={theme}>
+    <div>
+      asdf
+    </div>
+  </ThemeProvider>
+)
