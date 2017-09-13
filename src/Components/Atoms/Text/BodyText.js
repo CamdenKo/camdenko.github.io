39cd7beb751c5fceadc9265a006c7e38befdc65f
Camden Ko <camdenko@gmail.com>
2017-09-13 10:40:56 -0500

    some homescreen, body text responsive

--- src/Components/Atoms/Text/BodyText.js
+++ src/Components/Atoms/Text/BodyText.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { media } from '../../../theme'
+
 const Container = styled.div`
   text-align: left;
   font-size: 30px;
@@ -8,6 +10,11 @@ const Container = styled.div`
   line-height: 30px;
   font-weight: 100;
   color: ${({ theme }) => theme.white};
+  ${
+    media.phoneTablet`
+      font-size: 20px;
+    `
+  }
 `
 
 export default props => (
--- src/Components/Organisms/HomePage/RightHome.js
+++ src/Components/Organisms/HomePage/RightHome.js
@@ -5,17 +5,34 @@ import {
   Bio,
   X,
   Circle,
+  AbsolutePosition,
   RightBackground,
+  StaticRotator,
+  DifferentComponents,
 } from '../../../Components'
 import { media } from '../../../theme'
 
 const FlexContainer = styled.div`
   position: relative;
+  width: 100%;
+  height: 100%;
   ${
     media.phoneTablet`
       display: flex;
+      height: 286px;
       align-items: center;
       justify-content: center;
+    `
+  }
+`
+
+const TextWrapper = styled.div`
+  position: absolute;
+  bottom: 100px;
+  right: 130px;
+  ${
+    media.phoneTablet`
+      position: static;
     `
   }
 `
@@ -23,7 +40,14 @@ const FlexContainer = styled.div`
 export default () => (
   <RightBackground>
     <FlexContainer>
-      <Bio />
+      <TextWrapper>
+        <Bio />
+      </TextWrapper>
+      <AbsolutePosition top={200} left={470}>
+        <StaticRotator degrees={-10}>
+          <X />
+        </StaticRotator>
+      </AbsolutePosition>
     </FlexContainer>
   </RightBackground>
 )
