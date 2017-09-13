c5cc9ee207f33e5047df4e9256cc533aace65567
Camden Ko <camdenko@gmail.com>
2017-09-13 07:34:36 -0500

    got rid of unnecessary react components

--- src/Components/Atoms/Shapes/Line.js
+++ src/Components/Atoms/Shapes/Line.js
@@ -1,7 +1,6 @@
-import React from 'react'
 import styled from 'styled-components'
 
-const Line = styled.div`
+export default styled.div`
   margin: 0 10px 10px 0;
   box-sizing: border-box;
   height: 100%;
@@ -9,7 +8,3 @@ const Line = styled.div`
   box-shadow: 5px 0 0 0 ${({ theme }) => theme.black};
   border-radius: 10px;
 `
-
-export default () => (
-  <Line />
-)
