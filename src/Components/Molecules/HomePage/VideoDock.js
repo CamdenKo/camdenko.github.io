f93eb5183ced72ef2a915c83c246e34c8b4263a5
Camden Ko <camdenko@gmail.com>
2017-09-21 15:39:55 -0500

    basic video architecture

--- src/Components/Molecules/HomePage/VideoDock.js
+++ src/Components/Molecules/HomePage/VideoDock.js
@@ -3,23 +3,82 @@ import styled from 'styled-components'
 
 import {
   Video,
+  BodyText,
 } from '../../../Components'
 
-const videoW = 320
-const videoH = videoW / 16 * 9
+const videoW = 450
+const videoH = (videoW / 16) * 9
+
+const backgroundW = 500
 
 const Background = styled.div`
-  background-color: ${({ theme}) => theme.yellow};
-  height: 400px;
+  background-color: ${({ theme }) => theme.yellow};
+  height: 350px;
+  width: ${backgroundW}px;
+  display: flex;
+  justify-content: center;
+  position: relative;
 `
 
 const VideoWrapper = styled.div`
-  width: ${videoW};
-  height: ${videoH};
+  width: ${videoW}px;
+  height: ${videoH}px;
+  margin-top: ${(backgroundW - videoW) / 2}px;
 `
 
-export default props => (
-  <Background>
-    <VideoWrapper></VideoWrapper>
-  </Background>
-)
+const Name = BodyText.extend`
+  box-sizing: border-box;
+  position: absolute;
+  bottom: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: fit-content;
+  color: ${({ theme }) => theme.black};
+`
+
+const Next = styled.p`
+  position: absolute;
+  bottom: 7px;
+  right: 10px;
+  margin: 0;
+  cursor: pointer;
+`
+
+const Last = Next.extend`
+  right: auto;
+  left: 10px;
+`
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentVideoIndex: 0,
+    }
+  }
+
+
+  next() {
+    this.state.currentVideoIndex = (this.state.currentVideoIndex + 1) % this.props.videos.length
+  }
+
+  render() {
+    return (
+      <Background>
+        <VideoWrapper>
+          <Video src={this.props.videos[this.state.currentVideoIndex].src} />
+        </VideoWrapper>
+        <Last>
+          &lt;
+        </Last>
+        <Name>
+          {this.props.videos[this.state.currentVideoIndex].name}
+        </Name>
+        <Next>
+          &gt;
+        </Next>
+      </Background>
+    )
+  }
+}
+
